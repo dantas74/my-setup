c6aeb003c631da7339233a4a4e113f0745f132a5
matheus-dr <m47heusdr@protonmail.com>
2022-12-18 18:00:30 -0300

    Adding more software

--- configure.sh
+++ configure.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-[[ $(ansible --help &> /dev/null) -eq 0 ]] && ansible-playbook playbook.yml
+MESSAGE="Starting playbook, go take a coffe and enjoy automation :)"
 
-if [[ $? -ne 0 && $(ansible --help) &&  $? -eq 0 ]]; then
+[[ $(ansible --help &> /dev/null) -eq 0 ]] && \
+  echo $MESSAGE && sleep 2 && \
+  ansible-playbook playbook.yml
+
+if [[ $? -ne 0 ]]; then
   read -sp "Please insert your password: " PASSWORD
   echo
-  echo "Starting playbook, go take a coffe and enjoy automation :)"
+  echo $MESSAGE
   sleep 2
   ansible-playbook -e "ansible_become_password=$PASSWORD" playbook.yml
-else
+elif [[ $(ansible --help) &&  $? -ne 0 ]]; then
   echo "Installing Ansible"
   sudo apt update -y &> /dev/null
   sudo apt install ansible -y &> /dev/null
-  echo "Starting playbook, go take a coffe and enjoy automation :)"
+  echo $MESSAGE
   sleep 2
   ansible-playbook playbook.yml
 fi
