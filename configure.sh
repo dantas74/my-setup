fe8d5fc9ae353d7b48c51560318a4090adb98f95
matheus-dr <m47heusdr@protonmail.com>
2022-12-18 16:00:45 -0300

    apt keys with keyring not working

--- configure.sh
+++ configure.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ansible --help &> /dev/null
+[[ $(ansible --help &> /dev/null) -eq 0 ]] && ansible-playbook playbook.yml
 
-if [ $? -eq 0 ]; then
+if [[ $? -ne 0 && $(ansible --help) &&  $? -eq 0 ]]; then
   read -sp "Please insert your password: " PASSWORD
   echo
   echo "Starting playbook, go take a coffe and enjoy automation :)"
@@ -10,6 +10,7 @@ if [ $? -eq 0 ]; then
   ansible-playbook -e "ansible_become_password=$PASSWORD" playbook.yml
 else
   echo "Installing Ansible"
+  sudo apt update -y &> /dev/null
   sudo apt install ansible -y &> /dev/null
   echo "Starting playbook, go take a coffe and enjoy automation :)"
   sleep 2
