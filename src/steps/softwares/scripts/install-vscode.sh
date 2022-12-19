0a77eb6a8d03ac8f045404417c16dfbb29b7345d
matheus-dr <m47heusdr@protonmail.com>
2022-12-18 22:07:50 -0300

    fixing installations

---
+++ src/steps/softwares/scripts/install-vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
+
+sudo install -D -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -f /tmp/packages.microsoft.gpg
+
+sudo install -Dv /dev/null /usr/lock/vscode.lock
